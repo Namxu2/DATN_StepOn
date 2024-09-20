45cd7466add3b903828b9a8c902e95481105aacc
Namxu <nguyenthenam0401@gmail.com>
2024-09-20 15:22:49 +0700

    up v1

---
+++ src/main/java/com/fpoly/datn/Controller/Home.java
@@ -0,0 +1,14 @@
+package com.fpoly.datn.Controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class Home {
+    @GetMapping("/home")
+    public String home(Model model) {
+        model.addAttribute("message", "Welcome to Thymeleaf!");
+        return "view/Home";
+    }
+}
