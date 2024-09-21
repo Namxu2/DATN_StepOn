eaf8278240edecee14cf240a7e013364b1c2ce71
Namxu <nguyenthenam0401@gmail.com>
2024-09-21 23:18:44 +0700

    update package

---
+++ src/main/java/com/fpoly/datn/repository/BrandRepository.java
@@ -0,0 +1,22 @@
+package com.fpoly.datn.repository;
+
+
+import org.springframework.stereotype.Repository;
+@Repository
+public interface BrandRepository {
+
+//    public interface BrandRepository extends JpaRepository<Brand, Long> {
+//        Brand findByName(String name);
+//
+//        @Query(value = "SELECT * FROM brand b " +
+//                "WHERE b.id LIKE CONCAT('%',?1,'%') " +
+//                "AND b.name LIKE CONCAT('%',?2,'%') " +
+//                "AND b.status LIKE CONCAT('%',?3,'%')", nativeQuery = true)
+//        Page<Brand> adminGetListBrands(String id, String name, String status, Pageable pageable);
+//
+//        @Query(name = "getProductOrderBrands",nativeQuery = true)
+//        List<ChartDTO> getProductOrderBrands();
+//
+//    }
+
+}
